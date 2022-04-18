7a5a9029fed9982473c1c99c5bdc620d48fb8d68
Colin Gray <colin@smilesoftware.com>
2022-04-17 22:53:43 -0700

    Initial commit

---
+++ test/main.ts
@@ -0,0 +1,15 @@
+import request from 'supertest';
+
+import app from '../app/app';
+
+process.env.TEST_SUITE = 'main';
+
+// API routes tests
+describe('Basic server test', () => {
+  test('Should answer the GET / request with a 200 status code.', (done) => {
+    request(app).get('/').then((response) => {
+      expect(response.status).toBe(200);
+      done();
+    });
+  });
+});
